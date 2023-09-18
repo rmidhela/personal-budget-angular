7973732557dd51300e3702a39942fb472a11594f
Rakesh Midhela <rakeshmidhela@Rakeshs-Air.lan>
2023-09-17 23:22:13 -0400

    Loading data from json to populate pie chart

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT||3000;
+const fs = require('fs');
+
+app.use ('/', express.static('public'));
+
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    fs.readFile('data.json','utf8',(err,data) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send('Error reading budget data');
+        }
+        const budget = JSON.parse(data);
+        res.json(budget);
+    });
+});
+
+app.listen(port, () => {
+console.log(`Server is running on  port ${port}`)
+});
